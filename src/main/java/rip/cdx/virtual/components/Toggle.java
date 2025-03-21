5e1eb45d115548936382a79ce5e2e96a0434996b
CDX <80155611+CDX-1@users.noreply.github.com>
2025-03-21 08:42:03 -0400

    Added Toggle component

---
+++ src/main/java/rip/cdx/virtual/components/Toggle.java
@@ -0,0 +1,36 @@
+package rip.cdx.virtual.components;
+
+import net.minestom.server.event.inventory.InventoryPreClickEvent;
+import net.minestom.server.item.ItemStack;
+import rip.cdx.virtual.ui.component.UIComponent;
+import rip.cdx.virtual.ui.events.PreClickHandler;
+import rip.cdx.virtual.ui.events.VirtualEvent;
+import rip.cdx.virtual.ui.state.State;
+import rip.cdx.virtual.ui.virtual.Renderer;
+
+public class Toggle extends UIComponent implements PreClickHandler {
+    private final State<ItemStack> onState = new State<>();
+    private final State<ItemStack> offState = new State<>();
+    private final State<Boolean> toggleState = new State<>();
+
+    @Override
+    public void render(Renderer renderer) {
+        renderer.setItem(renderer.nextSlot(), subRenderer -> {
+            ItemStack onItem = subRenderer.getState(onState);
+            ItemStack offItem = subRenderer.getState(offState);
+            boolean toggled = subRenderer.getState(toggleState);
+
+            if (toggled)
+                return onItem;
+            else
+                return offItem;
+        });
+    }
+
+    @Override
+    public void onPreClick(VirtualEvent<InventoryPreClickEvent> virtualEvent) {
+        virtualEvent.getEvent().setCancelled(true);
+        boolean toggled = virtualEvent.getVirtual().getState(toggleState);
+        virtualEvent.getVirtual().setState(toggleState, !toggled);
+    }
+}
