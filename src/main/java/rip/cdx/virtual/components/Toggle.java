d7eb4b694dfb95d335f416690b04f563204f0dea
CDX <80155611+CDX-1@users.noreply.github.com>
2025-03-22 15:11:43 -0400

    Added onToggle to toggle component

--- src/main/java/rip/cdx/virtual/components/Toggle.java
+++ src/main/java/rip/cdx/virtual/components/Toggle.java
@@ -8,19 +8,27 @@ import rip.cdx.virtual.ui.events.VirtualEvent;
 import rip.cdx.virtual.ui.state.State;
 import rip.cdx.virtual.ui.virtual.Renderer;
 
+import java.util.function.Consumer;
+
 public class Toggle extends UIComponent implements PreClickHandler {
     private final State<ItemStack> onState = new State<>();
     private final State<ItemStack> offState = new State<>();
     private final State<Boolean> toggleState = new State<>();
+    private final State<Consumer<Boolean>> onToggle = new State<>();
 
-    public Toggle(ItemStack onItem, ItemStack offItem, boolean initialToggle) {
+    public Toggle(ItemStack onItem, ItemStack offItem, boolean initialToggle, Consumer<Boolean> onToggle) {
         this.onState.set(onItem);
         this.offState.set(offItem);
         this.toggleState.set(initialToggle);
+        this.onToggle.set(onToggle);
+    }
+
+    public Toggle(ItemStack onItem, ItemStack offItem, Consumer<Boolean> onToggle) {
+        this(onItem, offItem, false, onToggle);
     }
 
     public Toggle(ItemStack onItem, ItemStack offItem) {
-        this(onItem, offItem, false);
+        this(onItem, offItem, false, (_unused) -> {});
     }
 
     @Override
@@ -41,6 +49,7 @@ public class Toggle extends UIComponent implements PreClickHandler {
     public void onPreClick(VirtualEvent<InventoryPreClickEvent> virtualEvent) {
         virtualEvent.getEvent().setCancelled(true);
         boolean toggled = virtualEvent.getVirtual().getState(toggleState);
+        virtualEvent.getVirtual().getState(onToggle).accept(!toggled);
         virtualEvent.getVirtual().setState(toggleState, !toggled);
     }
 }
