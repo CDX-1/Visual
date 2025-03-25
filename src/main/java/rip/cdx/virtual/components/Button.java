56f5689e39a1d40ca4b35d718ccb4cda43c06bbf
CDX <80155611+CDX-1@users.noreply.github.com>
2025-03-25 12:59:22 -0400

    Added button component

---
+++ src/main/java/rip/cdx/virtual/components/Button.java
@@ -0,0 +1,47 @@
+package rip.cdx.virtual.components;
+
+import net.minestom.server.event.inventory.InventoryPreClickEvent;
+import net.minestom.server.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import rip.cdx.virtual.ui.component.UIComponent;
+import rip.cdx.virtual.ui.events.ComponentInitializeEvent;
+import rip.cdx.virtual.ui.events.ComponentUpdateEvent;
+import rip.cdx.virtual.ui.rendering.Renderer;
+import rip.cdx.virtual.ui.state.State;
+
+import java.util.function.Consumer;
+
+public class Button extends UIComponent {
+    private final State<ItemStack> item = new State<>();
+    private final State<Consumer<InventoryPreClickEvent>> action = new State<>();
+
+    public Button(int slot, ItemStack item, Consumer<InventoryPreClickEvent> action) {
+        super(slot);
+        this.item.setDefaultValue(item);
+        this.action.setDefaultValue(action);
+    }
+
+    public Button(ItemStack item, Consumer<InventoryPreClickEvent> action) {
+        this.item.setDefaultValue(item);
+        this.action.setDefaultValue(action);
+    }
+
+    @Override
+    public void onInitialize(ComponentInitializeEvent event) {
+        event.reserve(event.nextSlotIfNull(slot));
+    }
+
+    @Override
+    public void onUpdate(ComponentUpdateEvent event) {
+        event.setItem(
+                event.getReservedSlots().getFirst(),
+                item.get(event)
+        );
+    }
+
+    @Override
+    public void onPreClick(@Nullable UIComponent component, Renderer renderer, InventoryPreClickEvent event) {
+        if (component != this) return;
+        action.get(renderer).accept(event);
+    }
+}
